4b2a739592f055419f0a0cf10aec40d45542ef24
jhj <hj5014221@gmail.com>
2025-06-28 00:36:15 +0900

    updated

--- routes/assitant.js
+++ routes/assitant.js
@@ -2,6 +2,7 @@ import express from "express";
 import { auth } from "../middleware/auth.js";
 import { getUserSurveyResult } from "../DB/users.js";
 import { handleChatting } from "../lib/handleChatting.js";
+import { getSuggestionModelResponse } from "../lib/ai/suggestionModel.js";
 
 const router = express.Router();
 
@@ -40,5 +41,29 @@ router.post("/chat", auth,async (req, res, next) => {
     next(error);
   }
 });
+
+router.post("/get_suggestion",auth,async (req,res,next)=>{
+  try {
+    const {hasSchedule,schedule,clientDate,clientTime} = req.body;
+    const {success, result} = await getUserSurveyResult(req.user.id);
+    if(!success){
+      throw new Error("Failed to get user survey result.");
+    }
+    const userProfileInfo = {
+      nickname: result[0].nickname,
+      likes: result[0].likes,
+      location: result[0].location,
+      personal_goal: result[0].personal_goal,
+      daily_outline: result[0].daily_outline,
+    };
+    const response = await getSuggestionModelResponse(hasSchedule,schedule,userProfileInfo,clientTime,clientDate);
+    console.log(response);
+    return res.status(200).json({
+      response,
+    });
+  } catch (error) {
+    next(error);
+  }
+})
 
 export default router;
