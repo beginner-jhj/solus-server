7e25cd7302dc94102222c3091e0acd8784a19f49
jhj <hj5014221@gmail.com>
2025-07-07 00:08:43 +0900

    mysql railway db config

--- DB/pool.js
+++ DB/pool.js
@@ -2,8 +2,9 @@ import "dotenv/config";
 import { createPool } from "mysql2/promise";
 
 export const pool = createPool({
-  host: "localhost",
-  user: "jhj",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
   password: process.env.DB_PW,
   database: process.env.DB_NAME,
 });
